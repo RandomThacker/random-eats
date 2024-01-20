fbcc9751c6f0e265b6c931f17e26cce7441f8a84
RandomThacker <randomthacker@gmail.com>
2024-01-21 00:59:15 +0530

    updated git link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -105,9 +105,11 @@ export function Header() {
               </Badge>
             </Button>
           </NavLink>
-          <Button variant="gradient" size="sm">
-            Github
-          </Button>
+          <Link to="https://github.com/RandomThacker/random-eats">
+            <Button variant="gradient" size="sm">
+              Github
+            </Button>
+          </Link>
         </div>
         <IconButton
           variant="text"
@@ -122,37 +124,38 @@ export function Header() {
           )}
         </IconButton>
         <NavLink to="/cart" className="sm:hidden block">
-            <Button variant="text" size="sm" color="blue-gray">
-              <Badge
-                content={cartItems.length}
-                className={cartItems.length === 0 ? "hidden" : ""}
+          <Button variant="text" size="sm" color="blue-gray">
+            <Badge
+              content={cartItems.length}
+              className={cartItems.length === 0 ? "hidden" : ""}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                  />
-                </svg>
-              </Badge>
-            </Button>
-          </NavLink>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </Badge>
+          </Button>
+        </NavLink>
       </div>
-      
+
       <Collapse open={openNav}>
         <NavList />
         <div className="flex w-full flex-nowrap items-center justify-between gap-2 lg:hidden">
-
-          <Button variant="gradient" className="w-[100%]">
-            Github
-          </Button>
+          <Link to="https://github.com/RandomThacker/random-eats">
+            <Button variant="gradient" className="w-[100%]">
+              Github
+            </Button>
+          </Link>
         </div>
       </Collapse>
     </Navbar>
