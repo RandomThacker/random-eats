c9d255391f94872cde1801da717c9a4e30e9110f
Aryan Thacker <141705990+RandomThacker@users.noreply.github.com>
2024-02-02 11:34:59 +0530

    Update constants.js

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -3,3 +3,16 @@ export const MENU_IMAGE = "https://res.cloudinary.com/swiggy/image/upload/fl_los
 export const CDN_URL ="https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 export const MENU_URL =
 "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+
+
+// export const API_URL =
+//   "https://corsproxy.org/?" +
+//   encodeURIComponent(
+//     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+//   );
+
+// export const MENU_API =
+//   "https://corsproxy.org/?" +
+//   encodeURIComponent(
+//     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.51981990724166&lng=73.86026275822753&restaurantId="
+//   );
